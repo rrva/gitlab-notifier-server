28f5be6fdfa165ea6b4e1844dc54848bcd3f3f19
Ragnar Rova <ragnar.rova@gmail.com>
2022-09-10 16:31:21 +0200

    Replay old messages so clients can build state when connecting

--- src/main/kotlin/se/rrva/gitlab/pipeline/notifier/EventBus.kt
+++ src/main/kotlin/se/rrva/gitlab/pipeline/notifier/EventBus.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 object EventBus {
-    private val internalEvents = MutableSharedFlow<Event>(0, 10, BufferOverflow.DROP_OLDEST)
+    private val internalEvents = MutableSharedFlow<Event>(10, 10, BufferOverflow.DROP_OLDEST)
     val events = internalEvents.asSharedFlow()
 
     fun produceEvent(event: Event): Boolean {
