690cb8cde862f9fe93aef017a620eef6f55e0c08
Ragnar Rova <ragnar.rova@gmail.com>
2022-09-10 16:30:08 +0200

    EventSocket can be reused, leading to connections who got silence

--- src/main/kotlin/se/rrva/gitlab/pipeline/notifier/EventSocket.kt
+++ src/main/kotlin/se/rrva/gitlab/pipeline/notifier/EventSocket.kt
@@ -10,46 +10,57 @@ import java.nio.ByteBuffer
 class EventSocket : WebSocketAdapter() {
     private val log = LoggerFactory.getLogger(this.javaClass)
     private val scope = CoroutineScope(SupervisorJob())
-    private val job: Job = scope.launch {
-        EventBus.events.collect {
-            log.info("Sending to ${session?.remote?.remoteAddress}: ${it.payload}")
-            withContext(Dispatchers.IO) {
-                session?.remote?.sendString(it.payload)
-            }
-        }
-    }
-    private val pingJob = scope.launch {
-        while (true) {
-            try {
-                withContext(Dispatchers.IO) {
-                    val payload = ByteBuffer.wrap("You There?".toByteArray())
-                    session?.remote?.sendPing(payload)
-                }
-            } catch (e: Exception) {
-                log.error(e.message, e)
-            }
-            delay(10000)
-        }
-    }
+    @Volatile
+    private var job: Job? = null
+    @Volatile
+    private var pingJob: Job? = null
 
     override fun onWebSocketConnect(sess: Session) {
         super.onWebSocketConnect(sess)
+        job = scope.launch {
+            log.info("Starting event flow for ${session?.remote?.remoteAddress}")
+            EventBus.events.collect {
+                log.info("Sending to ${session?.remote?.remoteAddress}: ${it.payload}")
+                withContext(Dispatchers.IO) {
+                    session?.remote?.sendString(it.payload)
+                }
+            }
+        }
+        pingJob = scope.launch {
+            log.info("Starting keepalive for ${session?.remote?.remoteAddress}")
+            while (true) {
+                try {
+                    withContext(Dispatchers.IO) {
+                        val payload = ByteBuffer.wrap("You There?".toByteArray())
+                        session?.remote?.sendPing(payload)
+                    }
+                } catch (e: Exception) {
+                    log.error(e.message, e)
+                }
+                delay(10000)
+            }
+        }
+
         log.info("Socket connected: ${sess.remote.remoteAddress}")
     }
 
     override fun onWebSocketText(message: String) {
         super.onWebSocketText(message)
         if (message.lowercase().contains("bye")) {
-            job.cancel("client closed")
-            pingJob.cancel("client closed")
+            job?.cancel("client closed")
+            job = null
+            pingJob?.cancel("client closed")
+            pingJob = null
             session.close(StatusCode.NORMAL, "Thanks")
         }
     }
 
     override fun onWebSocketClose(statusCode: Int, reason: String?) {
         super.onWebSocketClose(statusCode, reason)
-        job.cancel("socket closed ${session.remote.remoteAddress}: [$statusCode] $reason")
-        pingJob.cancel("client closed")
+        job?.cancel("socket closed ${session.remote.remoteAddress}: [$statusCode] $reason")
+        job = null
+        pingJob?.cancel("client closed")
+        pingJob = null
         log.info("Socket Closed: [$statusCode] $reason")
     }
 
